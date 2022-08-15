56aed2323eb0d909a3dae1718b789e5ae661e42a
Mehmet Dilmen <mehmetdilmen03@gmail.com>
2022-08-15 10:13:05 +0300

    some bugs fix3

--- src/components/Stories.vue
+++ src/components/Stories.vue
@@ -17,9 +17,9 @@
       >
         <div
           class="story__source"
-          @mouseenter="!isPaused ? pauseStory($event) : null"
-          @mouseleave="isPaused ? playStory($event) : null"
-          @click="isPaused ? playStory($event) : pauseStory($event)"
+          @mouseenter="!isCntrlPaused ? pauseStory($event) : null"
+          @mouseleave="isCntrlPaused ? playStory($event) : null"
+          @click="isCntrlPaused ? playStory($event) : pauseStory($event)"
         >
           <video
             :id="getSrc(story, index).url"
@@ -134,6 +134,7 @@ export default {
     progress: 0,
     interval: 0,
     isPaused: false,
+    isCntrlPaused: false,
     newDur: 0,
     pausePer: 0,
   }),
@@ -270,6 +271,7 @@ export default {
         this.toggleVideo("pause", event);
       }
       this.isPaused = true;
+      this.isCntrlPaused = true;
       this.pausePer = this.percent;
       clearInterval(this.progress);
       clearInterval(this.interval);
@@ -279,6 +281,8 @@ export default {
       if (event) {
         this.toggleVideo("play", event);
       }
+      this.isCntrlPaused = true;
+
       this.isPaused = false;
       this.play();
     },
